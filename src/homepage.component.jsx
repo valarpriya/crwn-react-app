d9d7d08640d478e556f494bb0fe43fd4d21bf453
valarpriya <valarmathiit.vm@gmail.com>
2020-06-05 22:10:41 -0400

    added scss

---
+++ src/homepage.component.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import './homepage.styles.scss'
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <div className="title">
+                        <span className="subtitle">Hats</span>
+                    </div> 
+                 </div> 
+            </div> 
+            <div className="menu-item">
+                <div className="content">
+                    <div className="title">
+                        <span className="subtitle"> Jackets</span>
+                    </div> 
+                 </div> 
+            </div> 
+            <div className="menu-item">
+                <div className="content">
+                    <div className="title">
+                        <span className="subtitle"> Womens</span>
+                    </div> 
+                 </div> 
+            </div> 
+            <div className="menu-item">
+                <div className="content">
+                    <div className="title">
+                        <span className="subtitle"> Mens</span>
+                    </div> 
+                 </div> 
+            </div> 
+        </div> 
+    </div> 
+
+)
+ 
+export default HomePage;
